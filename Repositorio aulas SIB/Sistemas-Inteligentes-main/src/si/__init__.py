a640939bf871b90eb430853071f3572e0c5b45e3
Tiago Oliveira <tiago.laranjeira.oliveira@gmail.com>
2022-09-26 11:58:22 +0100

    commit aula

---
+++ Repositorio aulas SIB/Sistemas-Inteligentes-main/src/si/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Tiago Oliveira"
+__credits__ = ["Tiago Oliveira"]
+__license__ = "Apache License 2.0"
+__version__ = "0.0.1"
+__maintainer__ = "Tiago Oliveira"
+__email__ = "pg45476@alunos.uminho.pt"
