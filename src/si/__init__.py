52941ab4494704b12b73a2a5dbf005da54e41047
Tiago Oliveira <tiago.laranjeira.oliveira@gmail.com>
2022-09-27 19:17:27 +0100

    try

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Tiago Oliveira"
-__credits__ = ["Vítor Pereira, Fernando Cruz, Tiago Oliveira"]
+__credits__ = ["Tiago Oliveira"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "Tiago Oliveira"
